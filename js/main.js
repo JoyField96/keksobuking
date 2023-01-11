1f395f6c269b5c140edfd129ffc97c1256c569e9
Admin <mor.96@mail.ru>
2023-01-11 08:26:59 +0300

    добавлен сборщик - maiN в lintignore

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
-
 import {objectsArray} from './util.js';
+
 import {renderApartments} from './generate.js';
 import {apartmentTimeIn,apartmentTimeOut,apartmentType,apartmentPrice} from './form.js';
 renderApartments();
